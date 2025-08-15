edac9dc36670ce27b3722c62d7458e5c1867adeb
Georgi Chalakov <gchalakovmmi@gmail.com>
2025-08-15 23:59:50 +0300

    Added an initial version of the db package.

---
+++ db/db.go
@@ -0,0 +1,31 @@
+package db
+
+import (
+	"fmt"
+	"context"
+	"net/http"
+	"github.com/jackc/pgx/v5"
+)
+
+type ConnectionDetails struct {
+	User string
+	Password string
+	ServerIP string
+	Port int
+	Schema string
+}
+
+func WithDB(cd ConnectionDetails, handler func(w http.ResponseWriter, r *http.Request, conn *pgx.Conn)) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		url := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", cd.User, cd.Password, cd.ServerIP, cd.Port, cd.Schema)
+		conn, err := pgx.Connect(context.Background(), url)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, "Database error", http.StatusInternalServerError)
+			return
+		}
+		defer conn.Close(context.Background())
+
+		handler(w, r, conn)
+	}
+}
